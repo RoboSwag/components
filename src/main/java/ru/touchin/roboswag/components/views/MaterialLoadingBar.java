a5c658ca290ac9413d6201b8e1e919a387416fa1
Maxim bachinsky <mbachinskiyol@gmail.com>
2017-12-28 16:31:28 +0300

    Fix import

--- src/main/java/ru/touchin/roboswag/components/views/MaterialLoadingBar.java
+++ src/main/java/ru/touchin/roboswag/components/views/MaterialLoadingBar.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
